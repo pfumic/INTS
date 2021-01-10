ba8c85b14f54f39100f4968f9c0458b6ba5316db
Patrik <patrik.fumic@gmail.com>
2021-01-10 14:50:24 +0100

    First commit of base Windows Form project
    Foundation for INTS project

---
+++ INTS_MLNET_PROJEKT/Models/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INTS_MLNET_PROJEKT.Models
+{
+    class Review
+    {
+        public float HotelId { get; set; }
+        public float UserId { get; set; }
+        public float Rating { get; set; }
+    }
+}
---
+++ INTS_MLNET_PROJEKT/Services/ReviewService.cs
@@ -0,0 +1,48 @@
+﻿using INTS_MLNET_PROJEKT.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INTS_MLNET_PROJEKT.Services
+{
+    class ReviewService
+    {
+        public ReviewService() {
+
+        }
+
+        public List<Review> GetAllReviews() {
+            bool headers = true;
+            List<Review> allReviews = new List<Review>();
+            using (var reader = new StreamReader(@"C:\Users\Patrik Fumic\source\repos\INTS_MLNET_PROJEKT\INTS_MLNET_PROJEKT\Data\hotel_ratings_final.csv")) {
+
+                while (!reader.EndOfStream) {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    if (headers) {
+                        headers = false;
+                        continue;
+                    }
+
+                    Review tempSet = new Review {
+                        HotelId = float.Parse(values[0]),
+                        Rating = float.Parse(values[1]),
+                        UserId = float.Parse(values[2])
+                    };
+
+                    allReviews.Add(tempSet);
+                }
+
+                //foreach (var item in MLsets) {
+                //    Console.WriteLine(item.hotelId + " " + item.rating + " " + item.username);
+                //}
+            }
+
+            return allReviews;
+        }
+    }
+}
